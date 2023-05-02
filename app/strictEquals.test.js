c1d03ae7a1c29174b277c73bd39f9c1ff917003a
Marco Fratini <marcofratini@outlook.com>
2023-05-02 20:06:37 +0200

    Add 2 more test conditions to strictEquals function

--- app/strictEquals.test.js
+++ app/strictEquals.test.js
@@ -19,6 +19,27 @@ describe("Given the strictEquals function", () => {
 
       const valueReturned = strictEquals(valueA, valueB);
       const expectedValue = false;
+      expect(valueReturned).toBe(expectedValue);
+    });
+  });
+  describe("when it receives the values of 0 and -0", () => {
+    test("then it should return the value of true", () => {
+      const valueA = 0;
+      const valueB = -0;
+
+      const valueReturned = strictEquals(valueA, valueB);
+      const expectedValue = true;
+      expect(valueReturned).toBe(expectedValue);
+    });
+  });
+  describe("when it receives the values of -0 and 0", () => {
+    test("then it should return the value of true", () => {
+      const valueA = -0;
+      const valueB = 0;
+
+      const valueReturned = strictEquals(valueA, valueB);
+      const expectedValue = true;
+      expect(valueReturned).toBe(expectedValue);
     });
   });
 });
