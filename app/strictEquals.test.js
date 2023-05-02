13d7f34460dff91907e10f6a6c310fdbe7d1da9e
Marco Fratini <marcofratini@outlook.com>
2023-05-02 20:10:50 +0200

    Add 1 and '1' test condition

--- app/strictEquals.test.js
+++ app/strictEquals.test.js
@@ -42,4 +42,14 @@ describe("Given the strictEquals function", () => {
       expect(valueReturned).toBe(expectedValue);
     });
   });
+  describe("when it receives the value of 1 and '1'", () => {
+    test("then it should return the value of false", () => {
+      const valueA = 1;
+      const valueB = "1";
+
+      const valueReturned = strictEquals(valueA, valueB);
+      const expectedValue = false;
+      expect(valueReturned).toBe(expectedValue);
+    });
+  });
 });
