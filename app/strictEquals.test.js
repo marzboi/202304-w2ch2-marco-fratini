ea354f0438efbcf948618243ed27af1924740d2f
Marco Fratini <marcofratini@outlook.com>
2023-05-02 19:32:02 +0200

    Add first testing of function strictEquals

--- app/strictEquals.test.js
+++ app/strictEquals.test.js
@@ -1,1 +1,15 @@
-import { strictEqual } from "./strictEquals.js";
+import { strictEquals } from "./strictEquals.js";
+
+describe("Given the strictEquals function", () => {
+  describe("when it receives the values of 1 and 1", () => {
+    test("then it should return the value of true", () => {
+      const valueA = 1;
+      const valueB = 1;
+
+      const valueReturned = strictEquals(valueA, valueB);
+      const expectedValue = true;
+
+      expect(valueReturned).toBe(expectedValue);
+    });
+  });
+});
